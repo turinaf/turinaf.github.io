74db2958e2e67154e817b7731e63f6068b538360
Turi <40395348+turinaf@users.noreply.github.com>
2025-01-13 15:58:17 +0800

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
                   href="https://github.com/tovacinni/research-website-template"
                   className="underline hover:text-neutral-800 dark:hover:text-neutral-300 transition-colors"
                 >
-                  research-website-template
+                  template
                 </a>
               </p>
             </div>
